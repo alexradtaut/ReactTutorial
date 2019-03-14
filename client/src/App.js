dbdc51a2a3ac4b25f3d7ef247acd88addfa41048
xxx <xxx@yyy.com>
2019-03-14 12:56:19 +0200

    Add Post

--- client/src/App.js
+++ client/src/App.js
@@ -23,6 +23,7 @@ import AddEducation from "./components/add-credentials/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
 import NotFound from "./components/not-found/NotFound";
+import Posts from "./components/posts/Posts";
 
 import "./App.css";
 
@@ -90,6 +91,9 @@ class App extends Component {
                   path="/add-education"
                   component={AddEducation}
                 />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/feed" component={Posts} />
               </Switch>
               <Route exact path="/not-found" component={NotFound} />
             </div>
